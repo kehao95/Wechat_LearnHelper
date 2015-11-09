fc83991bf794131dc740ad9602780be4dcdc7a85
jy-Hamlet <huangjunyao0721@gmail.com>
2015-11-10 00:51:45 +0800

    first add

--- src/server.py
+++ src/server.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 import hashlib
 import sys
-sys.path+=["./lib","./lib/wechat-python-sdk"]
+
+try:
+    sys.path+=[".\\lib",".\\lib\\wechat-python-sdk"]
+except:
+    sys.path+=["./lib","./lib/wechat-python-sdk"]
 from wechat_sdk import WechatBasic
 from wechat_sdk.messages import (
     TextMessage, VoiceMessage, ImageMessage, VideoMessage, LinkMessage, LocationMessage, EventMessage
 )
 import logging
 
-
 _APP_TOKEN = '***REMOVED***'
-
+#requestCases = {'绑定': respBind,}
 
 # logger
 
@@ -22,6 +25,7 @@ logger.info("Info On")
 # wechat
 wechat = WechatBasic(token=_APP_TOKEN)
 app = Flask(__name__)
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -62,6 +66,10 @@ def check_SHA1(request):
         return False
 
 
+def respBind(customerID):
+    return "http://59.66.139.196:5000/bind"
+
+
 def handle_request(request):
     logger.debug("handle_request")
     signature = request.args.get('signature')
@@ -69,14 +77,31 @@ def handle_request(request):
     data = request.get_data().decode('utf-8')
     wechat.parse_data(data)
     message = wechat.get_message()
+    customerID = message.source
     response = ""
+
     if isinstance(message, TextMessage):
         print("TextMessage")
-        response = wechat.response_text(content="hello")
+        respCont = ""
+        try:
+            respCont = requestCases.get(message.content)(customerID)
+        except:
+            respCont = "指令无效"
+
+        response = wechat.response_text(content=respCont)
     else:
         print("OtherMessage")
         response = wechat.response_text(content="请输入文字信息")
     return response
+
+
+
+
+@app.route('/bind')
+def bindStAccount():
+    return render_template("bind.html")
+
+requestCases = {"绑定": respBind}
 
 
 if __name__ == '__main__':
