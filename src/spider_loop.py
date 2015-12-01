caa156dd4e78db620fea462961db72eaa5ce54b4
Ke Hao <kehao95@gmail.com>
2015-12-02 00:25:15 +0800

    spider get all info for update

--- src/spider_loop.py
+++ src/spider_loop.py
@@ -17,6 +17,9 @@ def get_users():
 async def update_database():
     logger.debug("database")
     users = get_users()
+    existing_works_ids = []
+    existing_messages_ids = []
+    existing_courses_ids = []
     Users = []
     for user in users:
         Users.append(User(user['username'], user['password']))
@@ -25,13 +28,35 @@ async def update_database():
     works = list(chain(*await asyncio.gather(*[course.works for course in courses])))
     messages = list(chain(*await asyncio.gather(*[course.messages for course in courses])))
 
-    worksids = [(work.id, work) for work in works]
-    messagesids = [(messages.id, messages) for messages in messages]
+
+    courses_to_append =[]
+    for course in courses:
+        if course.id not in existing_courses_ids:
+            existing_courses_ids.append(course.id)
+            courses_to_append.append(course)
+    logger.debug(len(courses_to_append))
+
+    works_to_append = []
+    for work in works:
+        if work.id not in existing_works_ids:
+            existing_works_ids.append(work.id)
+            works_to_append.append(work)
+    logger.debug(len(works_to_append))
+
+    messages_to_append = []
+    for message in messages:
+        if message.id not in existing_messages_ids:
+            existing_messages_ids.append(message.id)
+            messages_to_append.append(message)
+    logger.debug(len(messages_to_append))
+
+
+    courses_dicts = [course.__dict__ for course in courses_to_append]
+    messages_dicts = list(await asyncio.gather(*[message.dict for message in messages_to_append]))
+    works_dicts = list(await asyncio.gather(*[work.dict for work in works_to_append]))
     completion = [(work.user.username, work.id) for work in works if work.completion]
-    print(">>>")
-    print(worksids)
-    print(messagesids)
-    print(completion)
+    user_course = [(course.user.username, courses.id) for course in courses]
+    print("end")
     await asyncio.sleep(100)
 
 
