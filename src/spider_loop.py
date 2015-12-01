e8f42f8b266c0bbf3c96c654b289d81dd62654e1
Ke Hao <kehao95@gmail.com>
2015-12-01 23:30:22 +0800

    spider loop

--- src/spider_loop.py
+++ src/spider_loop.py
@@ -1,54 +1,45 @@
-import sys
-import json
-from json import JSONEncoder
+__author__ = "kehao"
+__email__ = "kehao95@gmail.com"
+
+from aiolearn import *
 import asyncio
-
-sys.path += ["./lib", "./lib/thu_learn"]
-import learn
-
-userQ = asyncio.Queue()
+import json
 
 
-async def update_users_list():
-    users = DB.GET_ALL_USERS()  # user.id user.pass
+def get_users():
+    logger.debug("get_users")
+    with open(".secret.json", 'r') as f:
+        secret = json.loads(f.read())
+        users = secret['users']
+    return users
+
+
+async def update_database():
+    logger.debug("database")
+    users = get_users()
+    Users = []
     for user in users:
-        userQ.put(user)
+        Users.append(User(user['username'], user['password']))
+    semesters = [Semester(user) for user in Users]
+    courses = list(chain(*await asyncio.gather(*[semester.courses for semester in semesters])))
+    works = list(chain(*await asyncio.gather(*[course.works for course in courses])))
+    messages = list(chain(*await asyncio.gather(*[course.messages for course in courses])))
+
+    worksids = [(work.id, work) for work in works]
+    messagesids = [(messages.id, messages) for messages in messages]
+    completion = [(work.user.username, work.id) for work in works if work.completion]
+    print(">>>")
+    print(worksids)
+    print(messagesids)
+    print(completion)
+    await asyncio.sleep(100)
 
 
-async def update_db():
-    async def update_messages_works():
-        """
-        this will update all the works and messages of courses in the DataBase
-        :return:
-        """
-        courses_id = DB.get_all_courses()
-        messages_id = DB.get_all_messages()
-        works_id  = DB.get_all_works()
-        for id in courses_id:
-            user = DB.get_vaild_user()
-            learn.login(user.username, user.password)
-            course = learn.Course(id)
-            for message in course.messages:
-                if message.id not in messages_id:
-                    m = JSON(message)
-                    DB.add_message(m)
-            for work in course.works:
-                if work.id not in works_id:
-                    w = JSON(work)
-                    DB.add_work(w)
-
-    async def update_users():
-        """
-        this will update all the user's homework status
-        :return:
-        """
-        users = DB.get_all_users()
-        for user in users:
-            login(user)
-            for work in
+async def main():
+    while True:
+        await  asyncio.gather(update_database())
 
 
-
-
-asyncio.Task(update_users_list())
-asyncio.Task(update_db())
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
