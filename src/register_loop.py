6795ed1da9101264368bddad86950ea63f4135e5
Ke Hao <kehao95@gmail.com>
2015-12-06 15:15:18 +0800

    add annotation to register loop

--- src/register_loop.py
+++ src/register_loop.py
@@ -12,7 +12,8 @@ database = None
 
 def get_users():
     """
-    获取刚刚绑定的用户列表
+    get users which registered moments ago
+    using file to transfer the data
     :return: users dict list
     """
     users = []
@@ -28,8 +29,13 @@ def get_users():
 
 
 def push_ok(users):
+    """
+    push registering done messages to server
+    data = {"type": "register_done", "users": users}
+    :param users:
+    :return: a request to server
+    """
     if not users:
-        # empty
         return None
     logger.debug("get_server_address")
     with open("address.txt", "r") as f:
@@ -38,43 +44,44 @@ def push_ok(users):
     requests.post(url, json.dumps(data))
 
 
-def get_test_users():
-    with open(".secret.json", 'r') as f:
-        users = json.loads(f.read())['users']
-    return users
-
-
 async def update_database():
-    #
-    logger.debug("database")
+    """
+    main register loop
+    fetch all information of the users' courses
+    push them to database
+    :return:
+    """
+    logger.debug("update database")
+
+    # prepare users
     users = get_users()
-    existing_works_ids = database.get_all_works()  # TODO
-    existing_messages_ids = database.get_all_messages()
-    existing_courses_ids = database.get_all_courses()
     Users = []
     for user in users:
         database.bind_user_openID(user['username'], user['password'], user['openid'])
         Users.append(User(user['username'], user['password']))
+
+    # fetch everything courses, works, messages ...
     semesters = [Semester(user) for user in Users]
     courses = list(chain(*await asyncio.gather(*[semester.courses for semester in semesters])))
     works = list(chain(*await asyncio.gather(*[course.works for course in courses])))
     messages = list(chain(*await asyncio.gather(*[course.messages for course in courses])))
-    logger.debug("checkpoint 1")
 
+    # pick out which are not in the database
+    existing_works_ids = database.get_all_works()  # TODO
+    existing_messages_ids = database.get_all_messages()
+    existing_courses_ids = database.get_all_courses()
     courses_to_append = []
     for course in courses:
         if course.id not in existing_courses_ids:
             existing_courses_ids.add(course.id)
             courses_to_append.append(course)
     logger.debug(" courses_to_append: %d" % len(courses_to_append))
-
     works_to_append = []
     for work in works:
         if work.id not in existing_works_ids:
             existing_works_ids.add(work.id)
             works_to_append.append(work)
     logger.debug("   works_to_append: %d" % len(works_to_append))
-
     messages_to_append = []
     for message in messages:
         if message.id not in existing_messages_ids:
@@ -82,33 +89,32 @@ async def update_database():
             messages_to_append.append(message)
     logger.debug("messages_to_append: %d" % len(messages_to_append))
 
+    # add them to database
     courses_dicts = [course.dict for course in courses_to_append]
     messages_dicts = list(await asyncio.gather(*[message.dict for message in messages_to_append]))
     works_dicts = list(await asyncio.gather(*[work.dict for work in works_to_append]))
-    completion = [(work.user.username, work.id) for work in works if work.completion]
     user_course = [(course.user.username, course.id) for course in courses]
-
-    logger.debug("insert     courses: %d" % len(courses_dicts))
-    logger.debug("insert    messages: %d" % len(messages_dicts))
-    logger.debug("insert       works: %d" % len(works_dicts))
-    logger.debug("insert   comletion: %d" % len(completion))
-    logger.debug("insert user_course: %d" % len(user_course))
-    # add this to databases
+    completion = [(work.user.username, work.id) for work in works if work.completion]
     database.add_courses(courses_dicts)
     database.add_messages(messages_dicts)
     database.add_works(works_dicts)
     database.update_completion(completion)
     database.add_user_course(user_course)
-    # push register success info
+    logger.debug("insert     courses: %d" % len(courses_dicts))
+    logger.debug("insert    messages: %d" % len(messages_dicts))
+    logger.debug("insert       works: %d" % len(works_dicts))
+    logger.debug("insert  completion: %d" % len(completion))
+    logger.debug("insert user_course: %d" % len(user_course))
+    # push register done messages
     push_ok(users)
-    print("wait for another 3 seconds")
-    await asyncio.sleep(3)
-    print("let's do it again!")
 
 
 async def main():
     while True:
         await  asyncio.gather(update_database())
+        print("wait for another 3 seconds")
+        await asyncio.sleep(3)
+        print("let's do it again!")
 
 
 if __name__ == "__main__":
